4876021be898430d452168329178805bb885f0f2
nabilaba <nabilazizbimaanggita@gmail.com>
2022-07-19 01:11:29 +0700

    fix async getting data

--- src/App.jsx
+++ src/App.jsx
@@ -19,23 +19,25 @@ function App() {
   const [newLink, setNewLink] = useState("");
   const [links, setLinks] = useState([]);
 
-  useEffect(() => {
-    const response = axios.get(`${URL.BASE_URL}${URL.SITES}`);
+  const getData = async () => {
+    const response = await axios.get(URL.BASE_URL + URL.SITES);
     setLinks(response.data);
+  };
+
+  useEffect(() => {
+    getData();
   }, []);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = axios.post(
-      `${URL.BASE_URL}${URL.SITES}`,
-      {
-        id: Date.now(),
-        original_link: newLink,
-        short_link: randomString(),
-      }
-    );
+    const response = await axios.post(`${URL.BASE_URL}${URL.SITES}`, {
+      id: Date.now(),
+      original_link: newLink,
+      short_link: randomString(6),
+    });
     setLinks([...links, response.data]);
   };
+  
   return (
     <Stack align="center" minH="100vh" h="full" justify={"center"}>
       <Container maxW="md">
@@ -56,19 +58,13 @@ function App() {
           </FormControl>
           <Button type="submit">Shorten</Button>
         </Stack>
-        {links.length > 0 && (
-          <Box mt={3}>
-            <Stack spacing={4}>
-              {links.map((link, index) => (
-                <HStack key={index}>
-                  <Text>
-                    {link.original_link} - {link.short_link}
-                  </Text>
-                </HStack>
-              ))}
-            </Stack>
-          </Box>
-        )}
+        {links.map((link, index) => (
+          <HStack key={index}>
+            <Text>
+              {link.original_link} - {link.short_link}
+            </Text>
+          </HStack>
+        ))}
       </Container>
     </Stack>
   );
