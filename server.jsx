98310ac35b37aef014a89a93690d1119e636e151
nabilaba <nabilazizbimaanggita@gmail.com>
2022-07-19 11:44:21 +0700

    be: added schedule for calling heroku

--- server.jsx
+++ server.jsx
@@ -1,8 +1,20 @@
+const cron = require("node-cron");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("database/pangkas-database.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
+const fetch = require("node-fetch");
+
+cron.schedule("* * * * *", async () => {
+  try {
+    await fetch("https://pangkas.herokuapp.com").then((res) => {
+      console.log(`Status: ${res.status}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
